50a2286110d1a86989ff7ed91b72dfd38af93df6
skishore <sriramkumar.kishorekumar@amd.com>
2025-07-17 15:32:37 +0000

    make fused adam as a jit module

---
+++ op_builder/fused_adam.py
@@ -0,0 +1,31 @@
+from .builder import CUDAOpBuilder
+
+import sys
+
+
+class FusedAdamBuilder(CUDAOpBuilder):
+    BUILD_VAR = 'APEX_BUILD_FUSED_ADAM'
+    NAME = "fused_adam_cuda"
+
+    def __init__(self):
+        super().__init__(name=self.NAME)
+
+    def absolute_name(self):
+        return f'apex.{self.NAME}'
+
+    def sources(self):
+        return ['apex/contrib/csrc/optimizers/fused_adam_cuda.cpp',
+                'apex/contrib/csrc/optimizers/fused_adam_cuda_kernel.cu']
+
+    def include_paths(self):
+        return ['contrib/csrc/']
+        
+    def cxx_args(self):
+        args = super().cxx_args()
+        return args + self.version_dependent_macros()
+
+    def nvcc_args(self):
+        nvcc_flags = ['-O3'] + self.version_dependent_macros()
+        if not self.is_rocm_pytorch():
+            nvcc_flags += ['--use_fast_math']
+        return nvcc_flags
