a388d4a1931c2bd608896719c6e4f3da8fa4b04e
skishore <sriramkumar.kishorekumar@amd.com>
2025-07-22 07:43:46 +0000

    make nccl p2p module as jit compile

---
+++ op_builder/nccl_p2p.py
@@ -0,0 +1,25 @@
+from .builder import CUDAOpBuilder
+
+import sys
+
+
+class NCCLP2PBuilder(CUDAOpBuilder):
+    BUILD_VAR = 'APEX_BUILD_NCCL_P2P'
+    NAME = "nccl_p2p_cuda"
+
+    def __init__(self):
+        super().__init__(name=self.NAME)
+
+    def absolute_name(self):
+        return f'apex.{self.NAME}'
+
+    def sources(self):
+        return ["contrib/csrc/nccl_p2p/nccl_p2p_cuda.cu",
+                "contrib/csrc/nccl_p2p/nccl_p2p.cpp"]
+
+    def include_paths(self):
+        return ['contrib/csrc/']
+        
+    def cxx_args(self):
+        args = super().cxx_args()
+        return args + self.version_dependent_macros() + self.generator_args()
